bc4a2c17bfcb29f9d60f62a3e74d1a66cac421a9
Camillo Bucciarelli <c.bucciarelli@reply.com>
2020-08-18 15:52:24 +0200

    fix auth interceptor

--- lib/http_client/authorization_interceptor.dart
+++ lib/http_client/authorization_interceptor.dart
@@ -3,21 +3,5 @@ import 'package:dio/dio.dart';
 
 const authClient = 'authClient';
 
-class AuthorizationInterceptor extends Interceptor {
-
-  @override
-  Future onRequest(RequestOptions options) async {
-//    final result = await getIt.get<UsersRepository>().userToken();
-//    result.fold((error) => log.i(error.message), (token) {
-//      try {
-//        options.headers.update(
-//            'Authorization',
-//                (_) => 'Bearer $token'
-//        );
-//      } catch (e) {
-//        options.headers.putIfAbsent('Authorization', () => 'Bearer $token');
-//      }
-//    });
-    return super.onRequest(options);
-  }
+abstract class AuthorizationInterceptor extends Interceptor {
 }
