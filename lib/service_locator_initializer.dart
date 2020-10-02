7c349259a117b07f96eadf1cdec75059b475d6d0
Camillo Bucciarelli <c.bucciarelli@reply.com>
2020-10-02 17:07:49 +0200

    Added navigator_service.dart

--- lib/service_locator_initializer.dart
+++ lib/service_locator_initializer.dart
@@ -1,6 +1,7 @@
 import 'package:connectivity/connectivity.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_core/router/core-router.dart';
+import 'package:flutter_core/router/navigator_service.dart';
 import 'package:get_it/get_it.dart';
 
 import 'error_mapping/app_error.dart';
@@ -11,7 +12,8 @@ void getItInitialization(CoreRouter appRouter, AppErrorDelegate errorDefaultMess
   getIt
     ..registerSingleton<Connectivity>(Connectivity(), signalsReady: true)
     ..registerSingleton<CoreRouter>(appRouter, signalsReady: true)
-    ..registerSingleton<AppErrorDelegate>(errorDefaultMessages, signalsReady: true);
+    ..registerSingleton<AppErrorDelegate>(errorDefaultMessages, signalsReady: true)
+    ..registerSingleton<NavigationService>(NavigationService(), signalsReady: true);
 
   dataSources();
   repositories();
