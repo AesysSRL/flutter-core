f1277d1c19f14d1d03cb6b474b68f923fbd4fc5f
Camillo Bucciarelli <Camillo.bucciarelli@aesys.tech>
2020-11-09 11:11:22 +0100

    •removed to model

--- lib/clean/entity.dart
+++ lib/clean/entity.dart
@@ -2,5 +2,4 @@ import 'package:flutter_core/clean/model.dart';
 import 'package:flutter_core/utilities/response_wrapper/serializable.dart';
 
 abstract class Entity<T extends Model> extends Serializable {
-  T toModel();
 }
