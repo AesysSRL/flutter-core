2a19bf0b50058e292f4d7017c17813550fdfa376
Camillo Bucciarelli <camillo.bucciarelli@aesys.tech>
2021-01-11 15:45:11 +0100

    • update core

--- lib/clean/use_case.dart
+++ lib/clean/use_case.dart
@@ -1,18 +1,20 @@
 import 'package:clean_architecture_core/error_mapping/app_error.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class AsyncUseCaseWithErrorHandling<I, R> {
+abstract class UseCase {}
+
+abstract class AsyncUseCaseWithErrorHandling<I, R> implements UseCase {
   Future<Either<AppError, R>> call([I param]);
 }
 
-abstract class AsyncUseCase<I, R> {
+abstract class AsyncUseCase<I, R> implements UseCase {
   Future<R> call([I param]);
 }
 
-abstract class SyncUseCaseWithErrorHandling<I, R> {
+abstract class SyncUseCaseWithErrorHandling<I, R> implements UseCase {
   Either<AppError, R> call([I param]);
 }
 
-abstract class SyncUseCase<I, R> {
+abstract class SyncUseCase<I, R> implements UseCase {
   R call([I param]);
 }
