eeb8839273d131b4405c44fbfc1f1e48854a646d
Camillo Bucciarelli <camillo.bucciarelli@aesys.tech>
2021-03-02 07:26:18 +0100

    added possibility to retrieve named instance of data source

--- lib/clean/repository.dart
+++ lib/clean/repository.dart
@@ -2,5 +2,7 @@ import 'package:clean_architecture_core/clean/data_source.dart';
 import 'package:clean_architecture_core/clean_architecture_core.dart';
 
 abstract class Repository<T extends DataSource> {
-   T dataSource = getIt<T>();
+   final T dataSource;
+
+   Repository({String dataSourceInstanceName}) : dataSource = getIt<T>(instanceName: dataSourceInstanceName);
 }
