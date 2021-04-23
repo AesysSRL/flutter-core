d8016c7d0c3c09562217952a9fd6aa95e4d10838
Camillo Bucciarelli <camillo.bucciarelli@aesys.tech>
2021-04-23 13:50:45 +0200

    refactored dio factory

--- lib/http_client/dio_factory.dart
+++ lib/http_client/dio_factory.dart
@@ -1,38 +1,38 @@
-import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 import '../error_mapping/network_exception.dart';
 
 class DioFactory {
   static const String _DEFAULT_DIO = 'default';
-  final Map<String, Dio> _dioInstances = {};
+  static final Map<String, Dio> _dioInstances = {};
 
   // ---------------------------------------------------------------------------
   //                                                    Singleton initialization
   // ---------------------------------------------------------------------------
   static final DioFactory _instance = DioFactory._();
 
-  DioFactory get instance => _instance;
+  static DioFactory get instance => _instance;
 
   DioFactory._();
 
   // ---------------------------------------------------------------------------
   //                                                       Dio instances manager
   // ---------------------------------------------------------------------------
-  DioFactory initialize() {
+  static DioFactory initialize() {
     newDioInstance(_DEFAULT_DIO, ErrorMapperInterceptor());
     return _instance;
   }
 
-  DioFactory newDioInstance(String dioInstanceName, Interceptor interceptor) {
+  static DioFactory newDioInstance(String dioInstanceName, Interceptor interceptor) {
     final dioInstance = Dio();
-    dioInstance.interceptors.add(LoggingInterceptors());
+    if(!kReleaseMode) dioInstance.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
     _dioInstances[dioInstanceName] = dioInstance..interceptors.add(interceptor);
     return _instance;
   }
 
-  Dio getDioInstance([String dioInstanceName = _DEFAULT_DIO]) {
+  static Dio getDioInstance([String dioInstanceName = _DEFAULT_DIO]) {
     if (_dioInstances.isEmpty) {
       throw Exception('no Dio has been evocated, have to call initilize() or newDioInstance()');
     }
@@ -42,7 +42,7 @@ class DioFactory {
     return _dioInstances[dioInstanceName]!;
   }
 
-  int getNumberOfDioInstances() {
+  static int getNumberOfDioInstances() {
     return _dioInstances.length;
   }
 }
@@ -56,45 +56,4 @@ class ErrorMapperInterceptor extends Interceptor {
         code: dioError.response?.statusCode ?? 999);
     super.onError(dioError, handler);
   }
-}
-
-class LoggingInterceptors extends Interceptor {
-  @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    log('--> ${options.method.toUpperCase()} ${'' + (options.baseUrl) + (options.path)}');
-    log('Headers:');
-    options.headers.forEach((k, v) => log('$k: $v'));
-    if (options.queryParameters.isNotEmpty) {
-      log('queryParameters:');
-      options.queryParameters.forEach((k, v) => log('$k: $v'));
-    }
-    if (options.data != null) {
-      logWrapped('Body: ${options.data}');
-    }
-    log('--> END ${options.method.toUpperCase()}');
-    super.onRequest(options, handler);
-  }
-
-  @override
-  void onError(DioError dioError, ErrorInterceptorHandler handler) {
-    log('<-- ${dioError.message} ${dioError.response}');
-    log('${dioError.response?.data != null ? dioError.response!.data : 'Unknown Error'}');
-    log('<-- End error');
-    super.onError(dioError, handler);
-  }
-
-  @override
-  void onResponse(Response response, ResponseInterceptorHandler handler) {
-    log('<-- ${response.statusCode} $response');
-    log('Headers:');
-    response.headers.forEach((k, v) => log('$k: $v'));
-    logWrapped('Response: ${response.data}');
-    log('<-- END HTTP');
-    super.onResponse(response, handler);
-  }
-}
-
-void logWrapped(String text) {
-  final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
-  pattern.allMatches(text).forEach((match) => log(match.group(0) ?? ''));
 }
