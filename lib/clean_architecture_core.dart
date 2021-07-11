20236f53177976fbb33bd9a0fae9a88410d54c16
Camillo Bucciarelli <camillo.bucciarelli@aesys.tech>
2021-07-11 15:43:22 +0200

    fix

--- lib/clean_architecture_core.dart
+++ lib/clean_architecture_core.dart
@@ -1,9 +1,13 @@
 library clean_architecture_core;
 
+import 'dart:developer';
+
 import 'package:clean_architecture_core/error_mapping/app_error.dart';
 import 'package:clean_architecture_core/router/core_navigator.dart';
 import 'package:connectivity/connectivity.dart';
+import 'package:devicelocale/devicelocale.dart';
 import 'package:get_it/get_it.dart';
+import 'package:intl/intl.dart';
 
 export 'package:clean_architecture_core/clean/core_provider.dart';
 export 'package:clean_architecture_core/clean/core_store.dart';
@@ -29,7 +33,9 @@ export 'package:clean_architecture_core/utilities/regex.dart';
 final getIt = GetIt.I;
 
 class Core {
-  static void init(AppErrorMessageDelegate appErrorMessageDelegate) {
+  static Future<void> init(AppErrorMessageDelegate appErrorMessageDelegate) async {
+    final deviceLocale = await Devicelocale.currentLocale;
+    Intl.withLocale(deviceLocale, () => log(deviceLocale!));
     getIt
       ..registerLazySingleton<CoreNavigator>(() => CoreNavigator(), instanceName: 'root')
       ..registerLazySingleton<Connectivity>(() => Connectivity())
