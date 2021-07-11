fed80f3b789ecd0605dbf93d2a16e174f6eddc0e
Camillo Bucciarelli <camillo.bucciarelli@aesys.tech>
2021-07-11 14:24:30 +0200

    fix

--- lib/json_converters/only_date_converter.dart
+++ lib/json_converters/only_date_converter.dart
@@ -1,8 +1,8 @@
 import 'package:intl/intl.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-class OnlyDateJsonConverter implements JsonConverter<DateTime?, String?> {
-  const OnlyDateJsonConverter();
+class NullableOnlyDateJsonConverter implements JsonConverter<DateTime?, String?> {
+  const NullableOnlyDateJsonConverter();
 
   @override
   DateTime? fromJson(String? json) {
@@ -18,5 +18,19 @@ class OnlyDateJsonConverter implements JsonConverter<DateTime?, String?> {
       return DateFormat('yyyy-MM-dd').format(object);
     }
     return null;
+  }
+}
+
+class OnlyDateJsonConverter implements JsonConverter<DateTime, String> {
+  const OnlyDateJsonConverter();
+
+  @override
+  DateTime fromJson(String json) {
+    return DateFormat('yyyy-MM-dd').parse(json, true);
+  }
+
+  @override
+  String toJson(DateTime object) {
+    return DateFormat('yyyy-MM-dd').format(object);
   }
 }
