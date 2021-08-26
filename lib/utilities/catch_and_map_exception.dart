7358e24b800a2080cbc6f65f1ce6ca050340b45d
Camillo Bucciarelli <camillo.bucciarelli@aesys.tech>
2021-08-26 09:18:13 +0200

    fix

--- lib/utilities/catch_and_map_exception.dart
+++ lib/utilities/catch_and_map_exception.dart
@@ -4,7 +4,7 @@ import 'package:fpdart/fpdart.dart';
 import '../clean_architecture_core.dart';
 import '../error_mapping/app_error.dart';
 
-Future<Either<AppError, R>> checkConnectionAndCatchException<R>(R Function() call) async {
+Future<Either<AppError, R>> checkConnectionAndCatchException<R>(Future<R> Function() call) async {
   final connectivityResult = (await catchException(getIt<Connectivity>().checkConnectivity)).fold((l) => ConnectivityResult.none, (r) => r);
   if (connectivityResult == ConnectivityResult.none) {
     return Either.left(AppError.offline());
@@ -12,28 +12,22 @@ Future<Either<AppError, R>> checkConnectionAndCatchException<R>(R Function() cal
   return catchException(call);
 }
 
-Future<Either<AppError, R>> catchException<R>(R Function() call, {AppErrorFromExceptionFunction? onError}) async {
-  return Either.tryCatch(() => call(), (e, _) => onError?.call(e) ?? AppError.fromException(e));
-  // try {
-  //   final result = await call();
-  //   return Right(result);
-  // } catch (e) {
-  //   if(onError != null){
-  //     return Left(onError(e));
-  //   }
-  //   return Left(AppError.fromException(e));
-  // }
+Future<Either<AppError, R>> checkConnectionAndCatchExceptionSync<R>(R Function() call) async {
+  final connectivityResult = (await catchException(getIt<Connectivity>().checkConnectivity)).fold((l) => ConnectivityResult.none, (r) => r);
+  if (connectivityResult == ConnectivityResult.none) {
+    return Either.left(AppError.offline());
+  }
+  return catchExceptionSync(call);
+}
+
+Future<Either<AppError, R>> catchException<R>(Future<R> Function() call, {AppErrorFromExceptionFunction? onError}) async {
+  try {
+    return Either.of(await call());
+  } catch (e) {
+    return Either.left(onError?.call(e) ?? AppError.fromException(e));
+  }
 }
 
 Either<AppError, R> catchExceptionSync<R>(R Function() call, {AppErrorFromExceptionFunction? onError}) {
   return Either.tryCatch(() => call(), (e, _) => onError?.call(e) ?? AppError.fromException(e));
-  // try {
-  //   final result = call();
-  //   return Right(result);
-  // } catch (e) {
-  //   if(onError != null){
-  //     return Left(onError(e));
-  //   }
-  //   return Left(AppError.fromException(e));
-  // }
 }
