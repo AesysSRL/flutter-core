dcd5a1328f3ce3ccf752fe1b45517229b6aa50fa
Giorgia Di Placido <giorgia.diplacido@aesys.tech>
2022-05-20 17:25:06 +0200

    General update of the Flutter Core packages

--- lib/dependency_injection/injector_configuration.config.dart
+++ lib/dependency_injection/injector_configuration.config.dart
@@ -4,24 +4,24 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
-import 'package:connectivity_plus/connectivity_plus.dart' as _i3;
-import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart' as _i2;
+import 'package:connectivity_plus/connectivity_plus.dart' as i3;
+import 'package:get_it/get_it.dart' as i1;
+import 'package:injectable/injectable.dart' as i2;
 
-import '../clean_architecture_core.dart' as _i4;
+import '../clean_architecture_core.dart' as i4;
 import 'injector_configuration.dart'
-    as _i5; // ignore_for_file: unnecessary_lambdas
+    as i5; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
-Future<_i1.GetIt> $initInjector(_i1.GetIt get,
-    {String? environment, _i2.EnvironmentFilter? environmentFilter}) async {
-  final gh = _i2.GetItHelper(get, environment, environmentFilter);
+Future<i1.GetIt> $initInjector(i1.GetIt get,
+    {String? environment, i2.EnvironmentFilter? environmentFilter}) async {
+  final gh = i2.GetItHelper(get, environment, environmentFilter);
   final externalLibraries = _$ExternalLibraries();
-  gh.factory<_i3.Connectivity>(() => externalLibraries.cbor);
-  await gh.factoryAsync<_i4.SharedPreferences>(() => externalLibraries.prefs,
+  gh.factory<i3.Connectivity>(() => externalLibraries.cbor);
+  await gh.factoryAsync<i4.SharedPreferences>(() => externalLibraries.prefs,
       preResolve: true);
   return get;
 }
 
-class _$ExternalLibraries extends _i5.ExternalLibraries {}
+class _$ExternalLibraries extends i5.ExternalLibraries {}
