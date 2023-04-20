89846e09efd2ec384551aaaca82f9f48dd152b1c
Anchini Andrea <andrea.anchini@aesys.tech>
2023-04-20 16:06:07 +0200

    fix

--- lib/notification/firebase_messaging_configure.dart
+++ lib/notification/firebase_messaging_configure.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -23,5 +21,5 @@ void configureFirebaseMessaging(Function(RemoteMessage) onNotificationReceived) 
   }
 
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  
+
 }
